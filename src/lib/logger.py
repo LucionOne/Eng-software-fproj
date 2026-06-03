6d5f486578788fd12e4b548720b3b36d139e07b0
Lucion <Nahh>
2026-06-03 01:31:38 -0300

    Update .gitignore and add logging setup functionality

---
+++ src/lib/logger.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+def setup_logging(log_file="logs\\DEBUG.log", warning_file="logs\\WARNING.log"):
+    """Generates the root logger and the default config"""
+    formatter = logging.Formatter(
+        "%(asctime)s  %(levelname)-8s  %(name)s — %(message)s"
+    )
+
+    # Console handler
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+
+    # File handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(warning_file)
+    file_handler.setLevel(logging.WARNING)
+    file_handler.setFormatter(formatter)
+
+    # Root logger
+    root = logging.getLogger()    
+    root.setLevel(logging.DEBUG)        
+    root.addHandler(console)
+    root.addHandler(file_handler)
