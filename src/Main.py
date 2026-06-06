50a92faa87ff93bf8956dda782a54080e76adba5
Lucion <Nahh>
2026-06-05 21:48:17 -0300

    main, for now, only used for testing

---
+++ src/Main.py
@@ -0,0 +1,17 @@
+from classes.DatabaseManager import DatabaseManager
+from classes.dataPuller import DataPuller
+import lib.logger as logger
+import time
+
+
+def main():
+    logger.setup_logging()
+    Puller = DataPuller()
+    Puller.Start()
+    time.sleep(30)
+
+
+
+
+if __name__ == '__main__':
+    main()
