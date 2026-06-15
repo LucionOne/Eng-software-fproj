271c9aadb1ef5830235b2595371f51b24e7a3d19
Lucion <Nahh>
2026-06-14 22:00:30 -0300

    Implement HTML response for dashboard

--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,22 @@
 from classes.DatabaseManager import DatabaseManager
 from classes.dataPuller import DataPuller
 import lib.logger as logger
+from classes.APIService import app_builder
+import uvicorn
 import time
+from typing import Generator
 
 
 def main():
-    logger.setup_logging()
-    Puller = DataPuller()
-    Puller.Start()
-    time.sleep(60)
+    # logger.setup_logging()
+    # Puller = DataPuller()
+    # Puller.Start()
+    # cont:bool = True
+    db = DatabaseManager()
+    app = app_builder(db)
+    uvicorn.run(app, host="0.0.0.0", port=3002)
 
+    
 
 
 
--- src/classes/APIService.py
+++ src/classes/APIService.py
@@ -6,23 +6,23 @@ from typing import Generator
 # uvicorn.run(app, host="0.0.0.0", port=3001)
 def app_builder(db:DatabaseManager) -> FastAPI:
     app = FastAPI(title= "TBD")
-    
-    
     def get_db() -> Generator[DatabaseManager]:
         yield db
     
     @app.get("/", response_class=HTMLResponse)
-    def get_html() -> None:
-        pass
+    def get_html() -> HTMLResponse:
+        with open("src\\assets\\dashboard.html", encoding='utf-8') as page:
+            html = page.read()
+        return HTMLResponse(html, status_code= 200)
     
     @app.get("/data")
     def get_data(db:DatabaseManager = Depends(get_db)) -> list[dict]:
-        data = db.fetch_data("SELECT * FROM sensor_logs")
-        return data
+        readings = db.fetch_data("SELECT * FROM sensor_logs")
+        return readings
     
     @app.get("/data/{date}")
     def get_data_since(date:str, db:DatabaseManager = Depends(get_db)) -> list[dict]:
-        data = db.fetch_data("SELECT * FROM sensor_logs WHERE recorded_at > ?",(date))
-        return data
+        readings = db.fetch_data("SELECT * FROM sensor_logs WHERE recorded_at > ?",(date))
+        return readings
     
     return app
